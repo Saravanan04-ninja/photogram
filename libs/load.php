ea51553283df7255bb80903cfe8d49d5447a9015
Saravanan <Saravanan234tk@gmail.com>
2026-08-12 05:28:52 +0000

    php photogram

--- libs/load.php
+++ libs/load.php
@@ -2,5 +2,5 @@
 
 function load_template($name){
 
-    include __DIR__. "/../_templates/$name.php"; //not consistent
+    include $_SERVER['DOCUMENT_ROOT']."/app/_templates/$name.php"; //not consistent
 }
